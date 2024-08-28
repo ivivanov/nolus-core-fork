b4f994a5810abdb1bf32704e9e42a3d0a8b5df05
ivivanov <i.ivanov@nolus.io>
2024-08-28 10:17:04 +0300

    wip

--- .github/actions/builder-create/entrypoint.sh
+++ .github/actions/builder-create/entrypoint.sh
@@ -11,13 +11,11 @@ AWS_ECR_REPOSITORY=$6
 DOCKERFILE=$7
 IMAGE_TAG=$8
 
-# Validate that all required variables are set
 if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" || -z "$AWS_REGION" || -z "$AWS_ECR_REGISTRY" || -z "$AWS_ECR_REPOSITORY" || -z "$DOCKERFILE" || -z "$IMAGE_TAG" ]]; then
   echo "Error: All arguments (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, AWS_ECR_REGISTRY, AWS_ECR_REPOSITORY, DOCKERFILE, IMAGE_TAG) must be provided."
   exit 1
 fi
 
-# Optionally configure the AWS CLI with these credentials
 aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 aws configure set region "$AWS_REGION"
